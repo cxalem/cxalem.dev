c9e50c7323a8fadb3b9d362864f8d9b4459f9a83
cxalem <smarketing.cuz@gmail.com>
2022-08-25 00:33:41 -0500

    new types added

--- types.ts
+++ types.ts
@@ -1,13 +1,26 @@
-export type Technology = string
+export type Technology = string;
 
 export type Project = {
-    title: string;
-    description: string;
-    technologies: Technology[];
-    repourl: string;
-    liveurl: string;
-  };
-  
-  export type DataSlug = {
-    [key: string]: Project
-  };
+  title: string;
+  description: string;
+  technologies: Technology[];
+  repourl: string;
+  liveurl: string;
+};
+
+export type DataSlug = {
+  [key: string]: Project;
+};
+
+export type JobDescription = {
+  company: string;
+  role: string;
+  description: string;
+  date: string;
+  company_link: string;
+  company_logo: string;
+};
+
+export type Job = {
+  [key: string]: JobDescription;
+};
